fb33e9114c14eb63025b1717418de3536cc54f35
Wanhella <gaizutytegabriele@gmail.com>
2025-10-16 19:19:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,79 +9,106 @@ using std::cout;
 using std::string;
 using std::vector;
 using std::accumulate;
+using std::endl;
+
 
 class Student{
 
-string firstName;
-string lastName;
-vector<int>grade;
-int exam;
-double finalResult;
-public:
-Student()
-{
-    firstName = "John";
-    lastName = "Doe";
-    grade = {1,2,3,4,5,6,7,7,9,6,7,5};
-    exam = 10;
-    res();
-};
-Student(string A, string B, vector<int>C, int D)
-{
-    firstName = A;
-    lastName = B;
-    grade = C;
-    exam = D;
-    res();
-};
-Student(const Student& studentObj)
-{
-    firstName = studentObj.firstName;
-    lastName = studentObj.lastName;
-    grade = studentObj.grade;
-    exam = studentObj.exam;
-    res();
-};
-Student& operator= (const Student& studentObj)
-{
-    firstName = studentObj.firstName;
-    lastName = studentObj.lastName;
-    grade = studentObj.grade;
-    exam = studentObj.exam;
-    res();
-    return *this;
-};
+    string firstName;
+    string lastName;
+    vector<int>grade;
+    int exam;
+    double finalResult;
+    public:
+    Student()
+    {
+        firstName = "John";
+        lastName = "Doe";
+        grade = {1,2,3,4,5,6,7,7,9,6,7,5};
+        exam = 10;
+        res();
+    };
+    Student(string A, string B, vector<int>C, int D)
+    {
+        firstName = A;
+        lastName = B;
+        grade = C;
+        exam = D;
+        res();
+    };
+    Student(const Student& studentObj)
+    {
+        firstName = studentObj.firstName;
+        lastName = studentObj.lastName;
+        grade = studentObj.grade;
+        exam = studentObj.exam;
+        res();
+    };
+    Student& operator= (const Student& studentObj)
+    {
+        firstName = studentObj.firstName;
+        lastName = studentObj.lastName;
+        grade = studentObj.grade;
+        exam = studentObj.exam;
+        res();
+        return *this;
+    };
 
-void res()
-{
-    finalResult = accumulate(grade.begin(), grade.end(), 0) / grade.size() * 0.4 + exam *0.6;
-}
-~Student()
-{
-    firstName.clear();
-    lastName.clear();
-    grade.clear();
-    exam = 0;
-    finalResult = 0;
-};
-void output()
-{
-    cout<< firstName <<" | "<< lastName <<" | "<< std::setprecision(2) << finalResult << std::endl;
+    void res()
+    {
+        finalResult = (static_cast<double>(accumulate(grade.begin(), grade.end(), 0)) / grade.size()) * 0.4 + exam * 0.6;;
+    }
+    ~Student()
+    {
+        firstName.clear();
+        lastName.clear();
+        grade.clear();
+        exam = 0;
+        finalResult = 0;
+    };
+    
+    void output()
+    {
+        cout<< firstName <<" | "<< lastName <<" | "<< std::setprecision(2) << finalResult << std::endl;
   
+    };
+    friend std::ostream &operator<<(std::ostream& out,const Student& studentObj);
+    friend std::istream &operator>>(std::istream& in , Student& studentObj);
 };
-friend std:: ostream& operator<<(std:: ostream& os ,const Student& studentObj);
-friend std:: istream& operator<<(std:: istream& is ,const Student& studentObj);
 
-};
- ostream &operator<<( ostream& out,const Student& studentObj)
+std::ostream &operator<<(std::ostream& out,const Student& studentObj)
 {  
-        out<<"Name "<<studentObj.firstName<<"Last Name "<<studentObj.lastName <<"Grade "<< studentObj.grade <<"Exam "<< studentObj.exam<<"Final result "<<res()<< endl;
-        return *out;
+    out << "First Name: " << studentObj.firstName
+        << " | Last Name: " << studentObj.lastName
+        << " | Grades: ";
+
+    for (int g : studentObj.grade)
+        out << g << " ";
+
+    out << "| Exam: " << studentObj.exam
+        << " | Final Result: " << std::fixed << std::setprecision(2) << studentObj.finalResult
+        << endl;
+
+    return out;
 };
- istream &operator>>( istream& in ,const Student& studentObj)
+std::istream &operator>>(std::istream& in , Student& studentObj)
 {
-        in>>studentObj.firstName>>studentObj.lastName>>studentObj.grade>>studentObj.exam;
-        return *in;
+   cout << "Enter first name, last name, number of grades, then grades, and exam: ";
+
+    int numGrades;
+    in >> studentObj.firstName >> studentObj.lastName >> numGrades;
+
+    studentObj.grade.clear();
+    for (int i = 0; i < numGrades; ++i) {
+        int g;
+        in >> g;
+        studentObj.grade.push_back(g);
+    }
+
+    in >> studentObj.exam;
+    studentObj.res();  // Recalculate final result
+
+    return in;
 };
 int main()
 {
