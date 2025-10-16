6b57a5eee290d5aa96dc53c266d7dac98b701e90
Wanhella <gaizutytegabriele@gmail.com>
2025-10-16 17:46:03 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include<vector>
 #include<numeric>
 
-using namespace std;
+
 using std::cout;
 using std::string;
 using std::vector;
@@ -19,47 +19,70 @@ double finalResult;
 public:
 Student()
 {
-  firstName = "John";
-  lastName = "Doe";
-  grade = {1,2,3,4,5,6,7,8,9,6,7,4};
-  exam = 10;
-  res();
-};;
+    firstName = "John";
+    lastName = "Doe";
+    grade = {1,2,3,4,5,6,7,8,9,6,7,4};
+    exam = 10;
+    res();
+};
 Student(string A, string B, vector<int>C, int D)
 {
-  firstName = A;
-  lastName = B;
-  grade = C;
-  exam = D;
-  res();
+    firstName = A;
+    lastName = B;
+    grade = C;
+    exam = D;
+    res();
+};
+Student(const Student& studentObj)
+{
+    firstName = studentObj.firstName;
+    lastName = studentObj.lastName;
+    grade = studentObj.grade;
+    exam = studentObj.exam;
+    res();
+};
+Student& operator= (const Student& studentObj)
+{
+    firstName = studentObj.firstName;
+    lastName = studentObj.lastName;
+    grade = studentObj.grade;
+    exam = studentObj.exam;
+    res();
+    return *this;
 };
 void res()
 {
-  finalResult = accumulate(grade.begin(), grade.end(), 0) / grade.size() * 0.4 + exam *0.6;
+    finalResult = accumulate(grade.begin(), grade.end(), 0) / grade.size() * 0.4 + exam *0.6;
 }
 ~Student()
 {
-  firstName.clear();
-  lastName.clear();
-  grade.clear();
-  exam = 0;
-  finalResult = 0;
+    firstName.clear();
+    lastName.clear();
+    grade.clear();
+    exam = 0;
+    finalResult = 0;
 };
 void output()
 {
-cout<< firstName <<" | "<< lastName <<" | "<< finalResult << std::endl;
+    cout<< firstName <<" | "<< lastName <<" | "<< finalResult << std::endl;
   
 };
 };
+
 int main()
 {
-  vector<Student>group;
-  for (int i = 0 ; i<5; i++)
-  {  
+    vector<Student>group;
+    for (int i = 0 ; i<5; i++)
+    {  
     Student student1;
     group.push_back(student1);
-  }
-  for(auto Z: group )Z.output();
-  
-  return 0;
+    }
+    for(auto Z: group )Z.output();
+    Student student1;
+    Student student2;
+    Student student3;
+    student2=student1;
+    student3=student1;
+    
+    return 0;
 }
